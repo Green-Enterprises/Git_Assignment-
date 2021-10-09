07c0b619bc6ef1f06be3e039d55c84aead99cc8e
UshanGallage99 <ushansanu@gmail.com>
2021-10-09 11:51:54 +0530

    Finished customer section close #2

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -16,7 +16,7 @@ public class AppInitializer extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/Customer.fxml"))));
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/Dashboard.fxml"))));
         primaryStage.show();
     }
 }
