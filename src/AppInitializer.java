a670452d368d69176b67d7098c08753104b23b9d
UshanGallage99 <ushansanu@gmail.com>
2021-10-09 09:47:05 +0530

    Added AppInitializer class

---
+++ src/AppInitializer.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * @author Ushan Gallage <ushansanu@gmail.com> (www.linkedin.com/in/ushan-gallage)
+ * @since 09/10/2021
+ */
+public class AppInitializer extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
