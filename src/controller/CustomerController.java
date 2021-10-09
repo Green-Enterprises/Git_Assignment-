cf3fe75a734b6140c399f16a327c2277e2f559ee
UshanGallage99 <ushansanu@gmail.com>
2021-10-09 10:48:30 +0530

    Create customer UI

---
+++ src/controller/CustomerController.java
@@ -0,0 +1,20 @@
+package controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+/**
+ * @author Ushan Gallage <ushansanu@gmail.com> (www.linkedin.com/in/ushan-gallage)
+ * @since 09/10/2021
+ */
+public class CustomerController {
+    public JFXTextField txtID;
+    public JFXTextField txtName;
+    public JFXTextField txtAddress;
+    public JFXTextField txtContact;
+    public Button btnCustomer;
+
+    public void btnCustomerOnAction(ActionEvent actionEvent) {
+    }
+}
